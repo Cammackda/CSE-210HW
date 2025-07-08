ac5e58a3bc98247421f6038c9ad9e14e74444c52
Cammackda <cammackda@byui.edu>
2025-07-08 17:12:13 -0600

    made a menu for the prgram file

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,40 @@
-using System;
+using Program;
+
+namespace Program;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+
+        int userResponse = -1;
+        while (userResponse != 5)
+        {
+            Console.WriteLine("1.) Start breathingh activity\n2.) Start reflecting activity\n3.) Start listing activity\n4.) Quit\n");
+            userResponse = int.Parse(Console.ReadLine());
+
+            switch (userResponse)
+            {
+                case 1:
+                    Entry newEntry = entryCreator.CreateJournal();
+                    journal.AddEntry(newEntry);
+                    break;
+                case 2:
+                    journal.DisplayEntries();
+                    break;
+                case 3:
+                    Console.Write("Enter filename to load from: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
+                    break;
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("That won't work; try again!");
+                    break;
+            }
+        }
     }
 }
