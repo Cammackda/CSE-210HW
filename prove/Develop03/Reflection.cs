a14edda294adaa7fd81b772c440d53742ee38834
Cammackda <cammackda@byui.edu>
2025-07-08 17:07:49 -0600

    added pauses to Reflection

--- prove/Develop03/Reflection.cs
+++ prove/Develop03/Reflection.cs
@@ -34,31 +34,31 @@ class Reflection
         string dateText = theCurrentTime.ToShortDateString();
         if (randomNum == 1)
             Console.WriteLine("Why was this experience meaningful to you?");
-
+            Thread.Sleep(3000);
         if (randomNum == 2)
             Console.WriteLine("Have you ever done anything like this before?");
-
+            Thread.Sleep(3000);
         if (randomNum == 3)
             Console.WriteLine("How did you get started?");
-
+            Thread.Sleep(3000);
         if (randomNum == 4)
             Console.WriteLine("How did you feel when it was complete?");
-
+            Thread.Sleep(3000);
         if (randomNum == 5)
             Console.WriteLine("What made this time different than other times when you were not as successful?");
-
+            Thread.Sleep(3000);
         if (randomNum == 6)
             Console.WriteLine("What is your favorite thing about this experience?");
-
+            Thread.Sleep(3000);
         if (randomNum == 7)
             Console.WriteLine("What could you learn from this experience that applies to other situations?");
-
+            Thread.Sleep(3000);
         if (randomNum == 8)
             Console.WriteLine("What did you learn about yourself through this experience?");
-        
+            Thread.Sleep(3000);
         if (randomNum == 9)
             Console.WriteLine("How can you keep this experience in mind in the future?");
-
+            Thread.Sleep(3000);
     }
 }
 
