cfe7b977f5614fcf6da1f16e06e215c7bd169bf7
Cammackda <cammackda@byui.edu>
2025-07-17 14:26:46 -0600

    updated this project

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,45 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+
+        int userResponse = -1;
+
+        while (userResponse != 6)
+        {
+            Console.WriteLine("1.) Create New Goal\n");
+            Console.WriteLine("2.) List Goals\n");
+            Console.WriteLine("3.) Save Goals\n");
+            Console.WriteLine("4.) Load Goals\n");
+            Console.WriteLine("5.) Record Event\n");
+            Console.WriteLine("6.) Quit");
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out userResponse);
+
+            switch (userResponse)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("That won't work; try again!");
+                    break;
+            }
+
+            Console.WriteLine("\nPress Enter to Return to the Menu");
+            Console.ReadLine();
+         }
     }
 }
